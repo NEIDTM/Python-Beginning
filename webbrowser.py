5b67a46fb1402b1c682ac32db4d22a61111e4966
Neid <104374612+NEIDTM@users.noreply.github.com>
2024-03-05 16:37:09 +0400

    Update webbrowser.py

--- webbrowser.py
+++ webbrowser.py
@@ -2,6 +2,8 @@ import requests
 import urllib
 import webbrowser
 
+task = input("Your request: ")
+
 webbrowser.register("chrome", None, webbrowser.BackgroundBrowser("C:\\Program Files\\Google\\Chrome\\Application\\chrome"))
 
-webbrowser.get(using="chrome").open_new_tab("github.com")
+webbrowser.get(using="chrome").open_new_tab("google.co.uk\search?q=" + task)
